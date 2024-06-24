bfd580233a0e53a8f8ab5b19ea7bf8cca9af6b31
C0B23134 <c0b2313484@edu.teu.ac.jp>
2024-06-24 18:02:52 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,13 +21,23 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+
+    bb_img = pg.Surface((20, 20))  # 20*20の黒いの範囲
+    bb_img.set_colorkey((0, 0, 0))  # 爆弾の黒い部分を透明に
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 半径10の赤色の円を描写
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)  # 爆弾の中心座標にx, yの値を設定
+    vx, vy = +5, +5  # 爆弾の横方向速度vx、縦方向速度vy
     clock = pg.time.Clock()
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        bb_rct.move_ip(vx, vy)  # move_ipは爆弾を動かす
+        screen.blit(bb_img, bb_rct)  #bb_img(爆弾)をbb_rct()の位置に表示
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
